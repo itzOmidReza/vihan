835702d861088d49f0f38d06cda618ea406e0abe
Omidreza <info@omidreza.website>
2025-12-18 18:23:16 +0330

    initial project

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Banana } from 'lucide-vue-next';
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+       <Button> <Banana /> Click me</Button>
+       <hr class="my-10">
+       <ModeToggle/>
+  </div>
+</template>
